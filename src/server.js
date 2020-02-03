8fb98ccadb48635aafa565bb00eea7cf3a50418c
Sarah Pickrahn <sarah.pickrahn@climate.com>
2020-02-03 10:40:50 -0800

    add server file

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const HTTP_PORT = 8000;
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
+
+// Insert here other API endpoints
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404);
+});
